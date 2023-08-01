04414144f9cf2504f8be59931d5eae054beb1201
bqnemdneektojg <141013246+bqnemdneektojg@users.noreply.github.com>
2023-08-01 14:06:12 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,33 @@
+
+#////////////////////// НАСТРОЙКИ БОТА \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+BOT_TOKEN = '6133392619:AAEdOpPeVTISe20_KMU3cBcwsyQjPP7Rl6c'# токен бота
+
+USERNAME = '@KnightProxy_bot' #юзернейм бота с @
+
+ADMIN = 6091250630 #ID админа [если несколько - через запятую]
+
+ADMIN_LINK = 'https://t.me/neuronqq' #Ссылка на админа
+
+CHANNEL_LINK = 'https://t.me/NewsKnight' #Ссылка на новостной канал
+
+
+
+#///////////////////// НАСТРОЙКИ API \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+PROXY_KEY = '6bQ3swG3E1y0gJ1IX9m0cVcEWjSvrrR3L1qetWKC' #Ключ proxyline.net
+
+QIWI_KEY = '0' #QIWI P2P ключ
+
+
+
+
+#//////////////////////// Картинки \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+MENU_IMAGE = 'https://i.imgur.com/rgfc5yJ.jpg' #Картинка в меню
+
+PROXY_IMAGE = 'https://i.imgur.com/rgfc5yJ.jpg' #Картинка в прокси воронке
+
+PROFILE_IMAGE = 'https://i.imgur.com/rgfc5yJ.jpg' #Картинка в профиле
+
+
